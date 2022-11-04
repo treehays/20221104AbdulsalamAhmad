ad2e1261599e869c182bbd6bce79bde0acfbc744
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-11-04 10:33:21 +0100

    First Question

---
+++ question1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace s3
+{
+    class Program
+    {
+        static int [] Results(int[] num, int original)
+        {
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num.Contains(original))
+                {
+                    original = original * 2;
+                }
+            }
+            return num;
+        }
+        public static void Main(string[] args)
+        {
+            int[] num = { 5, 3, 6, 1, 12 };
+            int original = 3;
+            // int[] num = { 2,7,9};
+            // int original = 4;
+            Console.WriteLine(Results(num, original));
+        }
+    }
+}
---
+++ question2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Lonely
+{
+    class Program
+    {
+        static int FinalValue(int[] num, int x)
+        {
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num.Contains(x))
+                {
+                    x = x * 2;
+                }
+            }
+            return x;
+        }
+        public static void Main(string[] args)
+        {
+            int[] num = { 5, 3, 6, 1, 12 };
+            int x = 3;
+            // int[] num = { 2,7,9};
+            // int original = 4;
+            Console.WriteLine(FinalValue(num, x));
+        }
+
+    }
+
+}
